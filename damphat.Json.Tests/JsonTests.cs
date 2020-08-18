609043249a5d93886d720eb4a3061250acded14f
Phat <damphat@gmail.com>
2020-08-18 19:59:56 +0700

    Add a xunit test

---
+++ damphat.Json.Tests/JsonTests.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Globalization;
+using Xunit;
+
+namespace damphat.Json.Tests
+{
+    public class JsonTests
+    {
+        [Theory]
+        [InlineData(null,"null")]
+        [InlineData(true,"true")]
+        [InlineData(false,"false")]
+        [InlineData(-90.09, "-90.09")]
+        [InlineData(double.PositiveInfinity, "Infinity")]
+        [InlineData(double.NegativeInfinity, "-Infinity")]
+        [InlineData(double.NaN,"NaN")]
+        public void StringifyPrimitive(object value, string expected)
+        {
+            Assert.Equal(expected, Json.JSON.Stringify(value));
+        }
+    }
+}
